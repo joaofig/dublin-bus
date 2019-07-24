f3846e17b469f3c6e715a8493afc7ecd1958169c
João Paulo Figueira <joao.figueira@gmail.com>
2019-07-24 13:09:43 +0100

    Updated code and notebooks

--- geo/df.py
+++ geo/df.py
@@ -52,6 +52,16 @@ class DataCleaner(object):
         df[self.dx_col] = np.insert(dist, 0, 0.0)
         return df
 
+    def get_max_speed(self, df: pd.DataFrame) -> float:
+        """
+        Calculates the maximum speed using the Tukey box plot algorithm
+        :param df: Source DataFrame
+        :return: Speed at the top whisker of the box plot
+        """
+        q = df[self.speed_col].quantile([.25, .5, .75])
+        iqr = q.loc[0.75] - q.loc[0.25]
+        return q.loc[0.75] + 1.5 * iqr
+
     def calculate_speed(self,
                         df: pd.DataFrame) -> pd.DataFrame:
         """
@@ -69,40 +79,7 @@ class DataCleaner(object):
 
     @staticmethod
     def get_type1_anomalies(df: pd.DataFrame) -> pd.DataFrame:
-
-        # Calculate the forward differences of latitude and longitude.
-        # One of the conditions for a type-1 anomaly to occur is to have both
-        # differences equal to zero.
-        df['dLat'] = df['Lat'].diff()
-        df['dLon'] = df['Lon'].diff()
-        df['dLat'] = df['dLat'].fillna(0.0)
-        df['dLon'] = df['dLon'].fillna(0.0)
-
-        # Now, shift both differences forward and backward so we can test for
-        # the type-1 anomaly using a single row. This will make the test a
-        # one-liner, and we can later remove the unnecessary columns.
-        df['dLatPrev'] = df['dLat'].shift(periods=1, fill_value=0.0)
-        df['dLonPrev'] = df['dLon'].shift(periods=1, fill_value=0.0)
-        df['dLatNext'] = df['dLat'].shift(periods=-1, fill_value=0.0)
-        df['dLonNext'] = df['dLon'].shift(periods=-1, fill_value=0.0)
-
-        anomalies = (df['dLat'] == 0.0) \
-                    & (df['dLon'] == 0.0) \
-                    & (df['dLatPrev'] != 0.0) \
-                    & (df['dLonPrev'] != 0.0) \
-                    & (df['dLatNext'] != 0.0) \
-                    & (df['dLonNext'] != 0.0)
-        df['type1'] = False
-        df.loc[anomalies, 'type1'] = True
-
-        df = df.drop(['dLat', 'dLon', 'dLatPrev', 'dLonPrev', 'dLatNext',
-                      'dLonNext'], axis=1)
-        return df
-
-    def get_anomalies(self,
-                      df: pd.DataFrame,
-                      max_speed: float) -> pd.DataFrame:
-        return df[df[self.speed_col] > max_speed]
+        return df[df['dx'] == 0.0]
 
     def calculate_derived_columns(self,
                                   df: pd.DataFrame) -> pd.DataFrame:
@@ -116,44 +93,10 @@ class DataCleaner(object):
         df = self.calculate_speed(df)
         return df
 
-    def calculate_anomalies(self,
-                            df: pd.DataFrame,
-                            max_speed: float):
-        df = self.calculate_derived_columns(df)
-        anomalies = self.get_anomalies(df, max_speed)
-        return df, anomalies
-
-    def fix_anomaly(self,
-                    df: pd.DataFrame,
-                    anom: pd.DataFrame) -> pd.DataFrame:
-        """
-        Fixes a type-1 anomaly
-        :param df: Source DataFrame
-        :param anom: Anomaly DataFrame - only the first will be fixed
-        :return: DataFrame with corrected anomaly
-        """
-        i1 = df.index.get_loc(anom.index[0])
-        i0 = i1 - 1
-        i2 = i1 + 1
-        idx2 = df.index[i2]
-        idx1 = df.index[i1]
-        idx0 = df.index[i0]
-
-        # Recalculate the time difference
-        df.loc[idx2, self.dt_col] += df.loc[idx1, self.dt_col]
-
-        # Recalculate the distance
-        lat1 = df.loc[idx0, self.lat_col]
-        lon1 = df.loc[idx0, self.lon_col]
-        lat2 = df.loc[idx2, self.lat_col]
-        lon2 = df.loc[idx2, self.lon_col]
-
-        df.loc[idx2, self.dx_col] = num_haversine(lat1, lon1, lat2, lon2)
-
-        # Recalculate the speed in km/h
-        df.loc[idx2, self.speed_col] = df.loc[idx2, self.dx_col] / \
-                                       df.loc[idx2, self.dt_col] * 3.6
-        return df
+    def calculate_type2_anomalies(self,
+                                  df: pd.DataFrame,
+                                  max_speed: float):
+        return df[df[self.speed_col] > max_speed]
 
     def fix_type1_anomaly(self,
                           df: pd.DataFrame,
@@ -193,11 +136,40 @@ class DataCleaner(object):
         :param df: Source DataFrame
         :return: DataFrame with corrected type-1 anomalies
         """
-        df = self.get_type1_anomalies(df)
-        anomalies = df[df['type1']]
-        for idx in anomalies.index:
-            df = self.fix_type1_anomaly(df, idx)
-        df = df[~df['type1']]
-        df = df.drop(['type1'], axis=1)
+        df = df[df['dx'] > 0.0].copy()
+        df = self.calculate_derived_columns(df)
         return df
 
+    def fix_type2_anomaly(self,
+                          df: pd.DataFrame,
+                          anom: pd.DataFrame) -> pd.DataFrame:
+        """
+        Fixes a type-2 anomaly
+        :param df: Source DataFrame
+        :param anom: Anomaly DataFrame - only the first will be fixed
+        :return: DataFrame with corrected anomaly
+        """
+        return self.fix_type1_anomaly(df, anom.index[0])
+
+    def fix_type2_anomalies(self,
+                            df: pd.DataFrame,
+                            max_speed: float) -> (pd.DataFrame, pd.DataFrame):
+        """
+        Detects and fixes type-2 anomalies (time travellers?)
+        :param df: Source DataFrame
+        :param max_speed: Maximum allowed speed for the bus
+        :return: Tuple with the cleaned DataFrame and the anomaly DataFrame
+        """
+        anomalies = None
+        type2 = self.calculate_type2_anomalies(df, max_speed)
+        while type2.shape[0] > 0:
+            df = self.fix_type2_anomaly(df, type2)
+            idx = type2.index[0]
+            row = df.loc[idx:idx].copy()
+            if anomalies is None:
+                anomalies = row
+            else:
+                anomalies = pd.concat([anomalies, row])
+            df = df.drop(index=idx)
+            type2 = self.calculate_type2_anomalies(df, max_speed)
+        return df, anomalies
